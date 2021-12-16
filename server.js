3103fd6c00af439923eb1c826e417aa2e71d98a9
rclank <53981655+rclank@users.noreply.github.com>
2021-12-16 13:35:18 -0500

    first query

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +7,23 @@ const app = express();
 // express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // your mysql username
+        user: 'root',
+        // your mysql password
+        password: 'bounce^Spar21',
+        database: 'election'
+    },
+    console.log('Connected to the election databse.')
+);
+
+db.query('SELECT * FROM candidates', (err, rows) => {
+    console.log(rows);
+});
 
 // default response for any other request (Not Found)
 app.use((req, res) => {
