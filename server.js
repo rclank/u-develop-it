def68b18c58da48c337f117671ff4e113479aa98
rclank <53981655+rclank@users.noreply.github.com>
2021-12-17 20:16:00 -0500

    party put route added

--- server.js
+++ server.js
@@ -109,6 +109,35 @@ app.post('/api/candidate', ({ body }, res) => {
     });
 });
 
+// update a candidate's party
+app.put('/api/candidate/:id', (req, res) => {
+    const errors = inputCheck(req.body, 'party_id');
+
+    if(errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+    
+    const sql = `UPDATE candidates SET party_id = ? WHERE id = ?`;
+    const params = [req.body.party_id, req.params.id];
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            // check if a record was found
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Candidate not found'
+            });
+        } else {
+            res.json({
+                message: 'success',
+                data: req.body,
+                changes: result.affectedRows
+            });
+        }
+    });
+});
+
 // get all parties
 app.get('/api/parties', (req, res) => {
     const sql = `SELECT * FROM parties`;
